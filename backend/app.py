f46fc4a5d387ee091330a95c4ba361f401a9f566
NuclearShamanHakunin <marko.jov101@gmail.com>
2025-08-12 19:55:47 +0200

    added required paths

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,6 +9,22 @@ db = SQLAlchemy(app)
 @app.route('/api')
 def hello():
     return "Hello from the backend!"
+
+
+@app.route('/choices', methods=['GET'])
+def get_choices():
+    return []
+
+
+@app.route('/choice', methods=['GET'])
+def get_choice():
+    return ""
+
+
+@app.route('/play', methods=['POST'])
+def play():
+    return ""
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
