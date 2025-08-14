5bf73001ae7e0b95b623cd3ed8b93af0e5880ce2
NuclearShamanHakunin <marko.jov101@gmail.com>
2025-08-14 21:26:25 +0200

    added explicit salt

--- backend/user.py
+++ backend/user.py
@@ -8,7 +8,11 @@ class User(db.Model):
     highscore = db.relationship('Highscore', back_populates='user', uselist=False)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(
+            password,
+            method='pbkdf2:sha256',
+            salt_length=16
+        )
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
