960552d58df464b5326ba0b024cbb30c9eeee893
NuclearShamanHakunin <marko.jov101@gmail.com>
2025-08-15 18:48:12 +0200

    removed unmaintained passlib

--- backend/app/user.py
+++ backend/app/user.py
@@ -1,19 +1,23 @@
-from passlib.context import CryptContext
+import bcrypt
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from .database import Base
 
-pwd_context = CryptContext(schemes=["bcrypt"])
 
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     username = Column(String(80), unique=True, nullable=False)
     password_hash = Column(String(256))
-    highscore = relationship('Highscore', back_populates='user', uselist=False)
+    highscore = relationship(
+        'Highscore', 
+        back_populates='user', 
+        uselist=False,
+        lazy='joined'
+    )
 
-    def set_password(self, password):
-        self.password_hash = pwd_context.hash(password)
+    def set_password(self, password: str):
+        self.password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-    def check_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
+    def check_password(self, password: str):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))
